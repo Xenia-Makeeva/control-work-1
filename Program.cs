4933503c882aa2200c6556062d1a419fba119b0c
Denis Makeev <dvm77701@yandex.ru>
2022-08-29 22:30:08 +0300

    скорректировала программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] mass;
+Console.WriteLine("Введите символы через запятую");
+string stroka = Console.ReadLine();
+mass = stroka.Split(',');
+var result = new string[mass.Length]; 
+var i = 0;
+foreach (var elem in mass )
+{
+    if (elem.Length <= 3)
+    {
+        result[i] = elem;
+        i++;
+    } 
+}
+Console.Write(string.Join(", ", result, 0, i));
+
